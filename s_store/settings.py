17051e6792b3a5e94835188ddfc402156b8786e1
Sergey <94781301+SergueiMoscow@users.noreply.github.com>
2023-05-28 23:15:29 +0300

    change db to mysql

--- s_store/settings.py
+++ s_store/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,12 +75,23 @@ WSGI_APPLICATION = 's_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'Larisa.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'Larisa.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, "db.cnf"),
+            "init_command": "SET default_storage_engine=INNODB",
+        }
     }
 }
+print(os.path.join(BASE_DIR, "../db.cnf"))
 
 
 # Password validation
